4efeca3fb68ca77a0d879eaebab94d93ad096661
Dilruba Khan <rubatanisa@yahoo.ie>
2021-10-28 01:34:33 +0100

    Header component and test

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -2,14 +2,16 @@ import React from 'react'
 import '../header/style.scss';
 import Logo from '../../assets/logo'
  
-const Header = (props)=> {
+const Header = (props) => {
     return (
-        <header>
-            <div className="logo">
-                <img src={Logo} alt="logo"></img>
+        <header data-test="headerComponent">
+            <div className="wrap">
+                <div className="logo">
+                    <img data-test="logoIMG" src={Logo} alt="Logo" />
+                </div>
             </div>
-            
         </header>
     )
-}
+};
+
 export default Header;
